d88696831bdf2d4ee4af27f7793031c783aa21bb
matiwos kebede <matiwoskebede01@gmail.com>
2025-12-20 14:04:14 +0300

    ArrayList created in 2 different way

---
+++ CollvsColl.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class CollvsColl{
+	public static void main(String[] args){
+		// creating arraylist using collection interface 
+		Collection<String> listArray = new ArrayList<>();
+		// create arraylist using List Interface
+		List<Integer> listNum = new ArrayList<>();
+		listNum.add(10);
+		System.out.println(listNum);
+
+	}
+}
