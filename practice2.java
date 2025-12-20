2bb32263eddfec10b409683ddfb1fabd7fe8f28a
matiwos kebede <matiwoskebede01@gmail.com>
2025-12-20 15:11:53 +0300

    hashmap created it like dictionary in python

---
+++ practice2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class pratice2{
+	public static void main(String[] args){
+		Map<String, Integer> ages = new HashMap<>();
+		ages.put("bati", 20);
+		ages.put("mami", 40);
+		ages.put("babi", 23);
+		ages.put("dadi", 55);
+		ages.put("fafi", 34);
+		System.out.println(ages);
+
+
+
+
+	}
+}
+		
