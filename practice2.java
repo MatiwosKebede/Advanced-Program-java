2a873cb9839054578d5841e4f29470c3e55df4cf
matiwos kebede <matiwoskebede01@gmail.com>
2025-12-20 15:35:38 +0300

    different data of hashmap manipulation and list key and value

--- practice2.java
+++ practice2.java
@@ -8,10 +8,19 @@ public class pratice2{
 		ages.put("babi", 23);
 		ages.put("dadi", 55);
 		ages.put("fafi", 34);
-		System.out.println(ages);
+		System.out.println("total hashmap created: "+ages);
+		System.out.println("get key by value .get(\"bati\"), bati age: " + ages.get("bati"));
+		//check existance by key
+		System.out.println("existance of mami by key, mami: " +  ages.containsKey("mami"));
+		//check existance by value
+		System.out.println("check existance by value: 20: " +ages.containsValue(20));
+		//remove by key
+		System.out.println("remove by key .remove(\"fafi\") by key");
+		ages.remove("fafi");
+		System.out.println("hashmap after update: " + ages);				
 
-
-
+	for(String key : ages.keySet())
+		System.out.println(key + ": " + ages.get(key));
 
 	}
 }
